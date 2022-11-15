009d877682b9fc0729c3dd50880a2bdf9d2cafde
Michael Davis <jrmdd1496@gmail.com>
2022-11-15 15:43:08 -0600

    Added routes for different pages on web application.

--- src/App.js
+++ src/App.js
@@ -1,10 +1,14 @@
-import logo from './logo.svg';
 import NavBar from './NavBar'
 import './App.css';
 
 function App() {
   return (
     <div className="App">
+      <NavBar />
+      <Routes>
+        <Route path="/" element={<Home />} />
+        <Route path="/JobForm" element={<Home />} />
+      </Routes>
     </div>
   );
 }
--- src/NavBar.js
+++ src/NavBar.js
@@ -2,7 +2,10 @@ import React from 'react'
 import {NavLink} from "react-router-dom"
 
 function NavBar () {
-
+    <div>
+        <NavLink to="/" className={"navlink"} >Home</NavLink>
+        <NavLink to="/JobForm" className={"navlink"} >New Job Form</NavLink>
+    </div>
 }
 
 export default NavBar;
