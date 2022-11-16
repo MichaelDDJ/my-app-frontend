86b017c58eee642ec8a2ccf64c5fbe29c1a8b4f6
Michael Davis <jrmdd1496@gmail.com>
2022-11-15 18:17:30 -0600

    commit tree 8d224d385f51e2d357c60b67d9a29c2a7fa5f2f4

--- src/App.js
+++ src/App.js
@@ -1,10 +1,15 @@
-import NavBar from './NavBar'
-import Home from './Home'
+import NavBar from './NavBar';
+import Home from './Home';
 import JobForm from './JobForm';
 import { Route, Routes} from 'react-router-dom';
+import React from 'react';
 import './App.css';
 
 function App() {
+
+  require('react-dom');
+window.React2 = require('react');
+console.log(window.React1 === window.React2);
   return (
     <div className="App">
       <NavBar />
